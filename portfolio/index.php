f94af244ab80fb96258dd43ee4985725a238bcc2
Galisteoo00 <115107104+Galisteoo00@users.noreply.github.com>
2023-10-11 09:04:34 +0200

    Update index.php

--- portfolio/index.php
+++ portfolio/index.php
@@ -22,7 +22,6 @@ $logros = $datos['logros'];
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title><?php echo $nombre . " " . $apellidos; ?> - Portafolio</title>
     <link rel="stylesheet" href="styles.css">
-    <!-- Agregar el enlace a la fuente de Google Fonts -->
     <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;700&display=swap" rel="stylesheet">
 </head>
 
