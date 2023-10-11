ebdb8c90eafe10d7da7bbb46a6fdaf70df325c3d
Galisteoo00 <115107104+Galisteoo00@users.noreply.github.com>
2023-10-11 09:00:03 +0200

    Add files via upload

---
+++ portfolio/config/config.php
@@ -0,0 +1,44 @@
+<?php
+$datos = array(
+    'nombre' => 'Adrián',
+    'apellidos' => 'Galisteo Cebrián',
+    'foto' => './img/fotoperfil.jpg',
+    'catprofesional' => 'Desarrollador Web',
+    'email' => 'adriaglisteo02@.com',
+    'telefono' => '+644850294',
+    'redessociales' => array(
+        'LinkedIn' => 'https://www.linkedin.com/in/tuusuario',
+        'Twitter' => 'https://twitter.com/tuusuario',
+    ),
+    'recursos' => 'Clica aquí para ir a los ejercicios',
+    'trabajos' => array(
+        array(
+            'titulo' => 'Analista de Ciberseguridad en Allpentesting',
+            'descripcion' => 'Trabajé como analista de ciberseguridad en Allpentesting, una gran consultoría de ciberseguridad a nivel nacional.',
+        ),
+    
+        array(
+            'titulo' => 'Proyecto 2',
+            'descripcion' => 'Descripción del proyecto 2.',
+        ),
+    ),
+    'habilidades' => array(
+        'Conocimientos avanzados en Ciberseguridad',
+        'PHP',
+        'HTML',
+        'CSS',
+        'JavaScript',
+        'MySQL',
+        'AWS',
+    ),
+    'logros' => array(
+        'Certificado en Desarrollo Web',
+        'Mención honorífica en Programación Avanzada',
+        'Tutor en cursos de ciberseguridad de la Junta de Andalucía (CEP)',
+        'Ponente en Ciberwall 2022',
+        'Certificación AWS'
+    ),
+    
+);
+?>
+
---
+++ portfolio/index.php
@@ -0,0 +1,114 @@
+<?php
+include "./config/config.php";
+$nombre = $datos['nombre'];
+$apellidos = $datos['apellidos'];
+$foto = $datos['foto'];
+$catprofesional = $datos['catprofesional'];
+$email = $datos['email'];
+$telefono = $datos['telefono'];
+$redessociales = $datos['redessociales'];
+$recursos = $datos['recursos'];
+$trabajos = $datos['trabajos'];
+$habilidades = $datos['habilidades'];
+$logros = $datos['logros'];
+?>
+
+<!DOCTYPE html>
+<html lang="es">
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title><?php echo $nombre . " " . $apellidos; ?> - Portafolio</title>
+    <link rel="stylesheet" href="styles.css">
+    <!-- Agregar el enlace a la fuente de Google Fonts -->
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;700&display=swap" rel="stylesheet">
+</head>
+
+<body>
+    <header>
+        <nav>
+            <ul>
+                <li><a href="#sobre-mi">Sobre mí</a></li>
+                <li><a href="#trabajos">Trabajos</a></li>
+                <li><a href="#habilidades">Habilidades</a></li>
+                <li><a href="#logros">Logros</a></li>
+                <li><a href="#contacto">Contacto</a></li>
+            </ul>
+        </nav>
+    </header>
+
+    <section id="sobre-mi">
+    <div class="container">
+        <!-- Agrega una clase "profile-image" para aplicar estilos CSS -->
+        <img src="<?php echo $foto; ?>" alt="Foto de perfil de <?php echo $nombre; ?>" class="profile-image">
+        <h1><?php echo $nombre . " " . $apellidos; ?></h1>
+        <p><?php echo $catprofesional; ?></p>
+        
+        <!-- Mueve el enlace a tus recursos aquí -->
+        <p><a href="<?php echo $recursos; ?>" target="_blank">Recursos</a></p>
+    </div>
+</section>
+
+
+
+    <section id="trabajos">
+    <div class="container">
+        <h2>Trabajos</h2>
+        <ul>
+            <?php foreach ($trabajos as $trabajo) { ?>
+                <li>
+                    <h3><?php echo $trabajo['titulo']; ?></h3>
+                    <p><?php echo $trabajo['descripcion']; ?></p>
+                </li>
+            <?php } ?>
+        </ul>
+    </div>
+</section>
+
+
+    <section id="habilidades">
+        <div class="container">
+            <h2>Habilidades</h2>
+            <ul>
+                <?php foreach ($habilidades as $habilidad) { ?>
+                    <li><?php echo $habilidad; ?></li>
+                <?php } ?>
+            </ul>
+        </div>
+    </section>
+
+    <section id="logros">
+    <div class="container">
+        <h2>Logros</h2>
+        <ul>
+            <?php foreach ($logros as $logro) { ?>
+                <li><?php echo $logro; ?></li>
+            <?php } ?>
+        </ul>
+    </div>
+</section>
+
+
+    <section id="contacto">
+    <div class="container">
+        <h2>Contacto</h2>
+        <p>Email: <?php echo $email; ?></p>
+        <p>Teléfono: <?php echo $telefono; ?></p>
+        <p><a href="<?php echo $redessociales['LinkedIn']; ?>" target="_blank">LinkedIn</a></p>
+        <p><a href="<?php echo $redessociales['Twitter']; ?>" target="_blank">Twitter</a></p>
+        
+        <p><a href="<?php echo $recursos; ?>" target="_blank">Recursos</a></p>
+    </div>
+</section>
+
+
+    <footer>
+    <div class="container">
+        <p>&copy; <?php echo date('Y'); ?> <?php echo $nombre; ?> - Todos los derechos reservados</p>
+    </div>
+</footer>
+
+</body>
+</html>
